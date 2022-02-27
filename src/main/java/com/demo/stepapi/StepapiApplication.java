96ce56ad5040f8eeb4ff1160eba2fee5a93463f4
Leopoldo Ramirez <rmontesleo@gmail.com>
2022-02-27 11:36:45 -0600

    Initial commit

---
+++ src/main/java/com/demo/stepapi/StepapiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.stepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/stepapi/StepapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.stepapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
